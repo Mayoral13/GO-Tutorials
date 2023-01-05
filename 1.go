94a74ef1f6053fc4df56a6362d9345464d651146
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-01-05 10:22:56 +0100

    Global Scoping

--- 1.go
+++ 1.go
@@ -1,12 +1,14 @@
 package main
  var a = "G"
  func main() {
- n() //PRINTS G
- m() //PRINTS O
- n() //PRINTS G
+ n()//PRINTS G
+ m()//PRINTS O
+ n()//PRINTS O
  }
- func n() { print(a) }
+ func n() {
+ print(a)
+ }
  func m() {
- a := "O"
+ a = "O"
  print(a)
  }
