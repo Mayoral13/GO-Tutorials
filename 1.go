7c895877be1593793f83255b364987b8ad7aeb34
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-01-07 00:47:19 +0100

    FizzBuzz Exercise

--- 1.go
+++ 1.go
@@ -1,15 +1,47 @@
-//Create a simple loop with the for construct. Make it loop 15 times and print out the loop
-//counter with the fmt package.
-
-//Rewrite this loop using goto. The keyword for may not be used now.
-package main 
+/*
+Write a program that prints the numbers from 1 to 100, but for multiples of three
+print “Fizz” instead of the number and for the multiples of five print “Buzz”. For
+numbers which are multiples of both three and five print “FizzBuzz”. (hint: use a
+switch with conditions)
+*/
+package main
 import("fmt")
-func loop15(){
-for i:= 0; i<15; i++{
-		fmt.Printf("Counter is: %d\n",i)
+const (
+    Fizz = 3;
+	Buzz = 5;
+	FizzBuzz = 15;
+)
+func ifc(){
+	for i:=0; i<=100; i++ {
+		if(i%Fizz == 0){
+			fmt.Printf("Fizz: %d\n",i)
+		}
+		if(i%Buzz == 0){
+			fmt.Printf("Buzz: %d\n",i)
+		}
+		if(i%FizzBuzz == 0){
+			fmt.Printf("FizzBuzz: %d\n",i)	
+		}else{
+			fmt.Printf("%d\n",i)
+		}
 	}
-
+}
+func swi(){
+	for i:=0; i<=100; i++ {
+		switch{
+		case i%Fizz == 0:
+			fmt.Printf("Fizz: %d\n",i)
+		case i%Buzz == 0:
+			fmt.Printf("Buzz: %d\n",i)
+		case i%FizzBuzz == 0:
+			fmt.Printf("FizzBuzz: %d\n",i)
+		default:fmt.Printf("%d\n",i)
+		
+		}
+	}
 }
 func main(){
-	loop15()
+	//USED BOTH SWITCH AND IF TO EXPERIMENT AND IF SEEMS TO BE THE BETTER CHOICE
+	swi(); //FIZZBUZZ DOESNOT WORK 
+	ifc(); //WORKS AS EXPECTED
 }
