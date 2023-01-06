fedfa8712f5273c1b5eb1c4cdb165e9043069061
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-01-06 23:48:46 +0100

    Switch statement

--- 1.go
+++ 1.go
@@ -1,20 +1,18 @@
+//Write a function Season which has as input-parameter a month-number and
+//which returns the name of the season to which this month belongs (disregard the day in the month).
 package main
-import("fmt";
-"runtime")
-var prompt = "Enter a digit, e.g. 3 "+ "or %s to quit."
-func windows(){
-if(runtime.GOOS == "windows"){
-	prompt = fmt.Sprintf(prompt,"Ctrl + Z, Enter");
-	fmt.Print(prompt)
-}
-}
-func linux(){
-	if(runtime.GOOS == "linux"){
-	prompt = fmt.Sprintf(prompt,"Ctrl + D, Exit");
-	fmt.Print(prompt)
+import("fmt")
+func Season(a uint){
+switch a {
+case 1:fmt.Printf("Summer\n")
+case 2:fmt.Printf("Winter\n")
+case 3:fmt.Printf("Autumn\n")
+default:fmt.Printf("Just a regular day\n")
 }
 }
 func main(){
-windows()
-linux()
+Season(1)
+Season(2)
+Season(3)
+Season(4)
 }
