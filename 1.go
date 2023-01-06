cdf0db2231f800ea94c7a82633a787d3c8c8c06d
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-01-06 09:03:50 +0100

    Wrapping runtime in functions

--- 1.go
+++ 1.go
@@ -2,16 +2,19 @@ package main
 import("fmt";
 "runtime")
 var prompt = "Enter a digit, e.g. 3 "+ "or %s to quit."
-func main(){
+func windows(){
 if(runtime.GOOS == "windows"){
 	prompt = fmt.Sprintf(prompt,"Ctrl + Z, Enter");
 	fmt.Print(prompt)
-}else{
-	prompt = fmt.Sprintf(prompt,"Ctrl + D, Enter");
-	fmt.Print(prompt);
-
 }
-
- 
-
+}
+func linux(){
+	if(runtime.GOOS == "linux"){
+	prompt = fmt.Sprintf(prompt,"Ctrl + D, Exit");
+	fmt.Print(prompt)
+}
+}
+func main(){
+windows()
+linux()
 }
