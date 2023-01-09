ce52ddf02b09f9d98f55eb55f093a7a33831a98d
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-01-09 19:56:47 +0100

    Blank

--- 1.go
+++ 1.go
@@ -1,14 +1,1 @@
-//Write a function which accepts 2 integers and returns their sum, product and difference. Make a
-//version with unnamed return variables, and a 2nd version with named return variables.
-package main
-import("fmt")
-func Calc(a,b uint){
-fmt.Printf("Sum is : %d\n",a+b)
-fmt.Printf("Product is : %d\n",a*b)
-fmt.Printf("Difference is : %d\n",a-b)
-}
-func main(){
-	Calc(6,5);
-	
-	
-}
+//
