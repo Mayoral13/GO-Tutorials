0d96cee0058e0b03c16b7335c5d57c894272fed8
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-01-12 00:59:53 +0100

    Celsius converter

--- 1.go
+++ 1.go
@@ -1,1 +1,22 @@
-//
+//PROGRAM THAT write a program that converts from Fahrenheit
+//into Celsius. (C = (F - 32) * 5/9)
+package main
+import "fmt"
+func main(){
+	var input float64;
+	var temp float64;
+	fmt.Print("WELCOME TO CELSIUS TEMP CONVERTER\n")
+	fmt.Print("Enter a value\n")
+	fmt.Scanf("%f",&input)
+	temp = (input-32)*5 / 9
+	fmt.Printf("The inputed value is: %f \n",input)
+	fmt.Print("Converted value is: ",temp)
+
+
+	
+
+
+
+}
+
+
