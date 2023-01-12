3a193fea102de239d560fb2bc3c9cd0269694cfa
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-01-12 01:02:15 +0100

    Converter put in a function

--- 1.go
+++ 1.go
@@ -2,21 +2,18 @@
 //into Celsius. (C = (F - 32) * 5/9)
 package main
 import "fmt"
+var input float64;
+var temp float64;
 func main(){
-	var input float64;
-	var temp float64;
+ Converter();
+}
+func Converter(){
 	fmt.Print("WELCOME TO CELSIUS TEMP CONVERTER\n")
 	fmt.Print("Enter a value\n")
 	fmt.Scanf("%f",&input)
 	temp = (input-32)*5 / 9
 	fmt.Printf("The inputed value is: %f \n",input)
 	fmt.Print("Converted value is: ",temp)
-
-
-	
-
-
-
 }
 
 
