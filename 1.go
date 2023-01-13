957d67d2a306a94c98a491dc4e91b9184dda726e
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-01-13 01:18:59 +0100

    Another one

--- 1.go
+++ 1.go
@@ -1,36 +1,24 @@
 
 /*
-Write a program that finds the smallest number
-in this list:
-x := []int{
- 48,96,86,68,
- 57,82,63,70,
- 37,34,83,27,
- 19,97, 9,17,
-}
+Write a function which takes an integer and
+halves it and returns true if it was even or false
+if it was odd. For example half(1) should return
+(0, false) and half(2) should return (1,
+true).
 */
 package main
-import("fmt")
+import "fmt"
+func half(a int) (int, bool){
+if(a %2 == 0){
+	b:= a / 2
+	return b,true
+}else{
+	b:= a / 2
+	return b,false;
+}
+}
 func main(){
-	var apple int;
-	x := []int{
-		48,96,86,68,
-		57,82,63,70,
-		37,34,83,27,
-		19,97,9,17,
-	   }
-	   //USED len(x)-1 because the comparison stops at the length of the array and since they are both looping
-	   //to the same length one has to be less than the other
-	   //Can decide to use math.Min() //but its for floats
-	   for i:=0; i<len(x)-1; i++{
-		for j:=0; j<len(x); j++{
-        if(x[i] < x[j]){
-			apple = x[i]
-		}else{
-			apple = x[j]
-		}
-		}
-	   }
-	   fmt.Printf("Smallest is %v\n",apple)
+	a,b := half(78)
+	fmt.Print("RESULT: ",a,b)
 }
 
