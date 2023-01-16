76d65ecc9052d67a8e42676f04eb08916b4fd87e
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-01-16 03:00:47 +0100

    Update endpoint is .....

--- 1.go
+++ 1.go
@@ -1,87 +1,74 @@
-//A CRUD API WITH A SLICE AS DATA STORAGE
+// CRUD API
 package main
-import(
-	//"fmt"
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 	"log"
-	"encoding/json"
-	"github.com/gorilla/mux"
-	//"strconv"
-	//"math/rand"
-
 )
-type Data struct{
-	Name string `json:"name"`
-	Occupation string `json:"occupation"`
-	Age string `json:"age"`
-	Id string `json:"id"`
-	User *User `json:"user"`
-}
-
-type User struct{
-	Name string `json:"name"`
-	Level string `json:"level"`
-}
-func create(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type","application/json")
+func Create(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","aplication/json")
 	var datax Data;
-	_ = json.NewDecoder(r.Body).Decode(&data);
-    data = append(data,datax)
-	json.NewEncoder(w).Encode(datax)
+	json.NewDecoder(r.Body).Decode(&datax)
+	data = append(data,datax);
+	json.NewEncoder(w).Encode(datax);
 }
-func getAll(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type","application/json")
-	json.NewEncoder(w).Encode(data)
+func Get(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","aplication/json")
+	json.NewEncoder(w).Encode(data);
 }
-func getOne(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type","application/json")
-	params := mux.Vars(r)
-	for _,item := range data{
-		if(item.Id == params["id"]){
-			json.NewEncoder(w).Encode(item)
-		}else{
-			http.Error(w,"NOT FOUND",http.StatusNotFound);
-		}
-	}
-}
-func delete(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type","application/json");
-	params :=mux.Vars(r)
-	for index,item := range data{
-		if (item.Id == params["id"]){
-        data = append(data[:index],data[index+1:]... )
-		break
-		}else{
-			http.Error(w,"NOT FOUND",http.StatusNotFound);
-		}
-	}
-}
-func update(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Conteent/Type","application/json")
+func Update(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","aplication/json")
 	params := mux.Vars(r)
 	for index,item := range data{
-		if(item.Id == params["id"]){
-			data = append(data[:index],data[index + 1:]...)
-			var datax Data
-			_ = json.NewDecoder(r.Body).Decode(&datax)
-			datax.Id = params["id"]
-			data = append(data,datax)
-			json.NewEncoder(w).Encode(datax)
-			return
+		if(item.ID == params["id"]){
+		data = append(data[:index],data[index+1:]... )
+		var dataxx Data;
+		json.NewDecoder(r.Body).Decode(&dataxx)
+		data = append(data,dataxx)
+		json.NewEncoder(w).Encode(data)
 		}
+		
 	}
-}
 
-var data []Data
-func main(){
-	r := mux.NewRouter();
-    data = append(data,Data{Name:"John",Occupation:"Doctor",Age:"24",Id:"1",User:&User{Name:"Cena",Level:"100"}})
-	r.HandleFunc("/create",create).Methods("POST")
-	r.HandleFunc("/update/{id}",update).Methods("PUT")
-	r.HandleFunc("/get",getAll).Methods("GET")
-	r.HandleFunc("/get/{id}",getOne).Methods("GET")
-	r.HandleFunc("/delete/{id}",delete).Methods("DELETE")
-	if err:=http.ListenAndServe(":3000",nil);err != nil{
-		log.Fatal(err)
+}
+func Delete(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","aplication/json")
+	params := mux.Vars(r)
+	for index,item := range data{
+		if(item.ID == params["id"]){
+			data = append(data[:index],data[index + 1:]... )
+			json.NewEncoder(w).Encode(data)
+		}
+		
 	}
+	
+}
+type Data struct{
+	ID string `json:"ID"`
+	Name string `json:"Name"`
+	Occupation string `json:"Occupation"`
+	Age string `json:"Age"`
+	User *User `json:"User"`
+}
+type User struct{
+	Name string `json:"Name"`
+	Level string `json:"Level"`
+}
+var data []Data;
+
+func main(){
+	data = append(data,Data{ID:"1",Name:"Kendrick",Occupation:"Rapper",Age:"24",User:&User{Name:"Lamar",Level:"10"}})
+	data = append(data,Data{ID:"2",Name:"Kendrick",Occupation:"Rapper",Age:"24",User:&User{Name:"Lamar",Level:"10"}})
+	data = append(data,Data{ID:"3",Name:"Kendrick",Occupation:"Rapper",Age:"24",User:&User{Name:"Lamar",Level:"10"}})
+
+	r := mux.NewRouter();
+	r.HandleFunc("/Create",Create).Methods("POST")
+	r.HandleFunc("/Get",Get).Methods("GET")
+	r.HandleFunc("/UpdKate/{id}",Update).Methods("PUT")
+	r.HandleFunc("/Delete/{id}",Delete).Methods("DELETE")
+	fmt.Print("SERVER IS LIVE")
+	log.Fatal(http.ListenAndServe(":3000",r))
 }
