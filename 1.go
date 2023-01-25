f92e4de6ee87e25049f4158e5d8243df92cfddbb
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-01-26 00:30:27 +0100

    Rock Paper Scissors program

--- 1.go
+++ 1.go
@@ -1,19 +1,78 @@
 /*
-2. Creating a dice-rolling simulator is a great beginner project to help you learn. In
-the real world, rolling dice allows you to generate a random number ranging from 1
-through 6, so your program should have the same functionality. This project uses the
-random module to generate random numbers and print them to the user.
+4. Rock, Paper, Scissors
+Rock, paper, scissors is a straightforward game in real life that’s helped solve arguments
+and determine who does what. However, coding a game of rock, paper, scissors is more
+complex than you might think, making it a great intermediate project for anyone.
+The rock, paper, scissors project uses a random number generator, similar to the dice
+rolling project. However, the project goes further by requiring you to code in the rules
+and logic of the game and create additional choices and elements for a user to pick.
 */
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
+	"strings"
 )
 func main(){
-Random()
+	fmt.Print("Welcome to a game of rock paper and scissors\n")
+	var userScore int;
+	var BotScore int;
+	var options = [3]string{"rock","paper","scissors"}
+	for (userScore < 5 || BotScore < 5 ) {
+	input := bufio.NewReader(os.Stdin)
+	fmt.Println("(Rock Paper or Scissors)")
+	text,_ :=input.ReadString('\n') 
+	pick := strings.TrimSpace(text);
+	if(pick == ""){
+		fmt.Println("Empty input retry")
+	}else if(pick != "rock" && pick != "paper" && pick != "scissors"){
+    fmt.Println("Invalid Input")
+	}else{
+	random :=rand.Intn(len(options));
+	BotPick := options[random];
+	fmt.Println("Bot Pick is : "+ BotPick);
+	fmt.Println("Your pick is : "+ pick);
+	if BotPick == "paper" && pick == "rock"{
+		fmt.Println("You lose!")
+		BotScore++;
+	   }else if BotPick == "paper" && pick == "scissors"{
+		fmt.Println("You win!")
+		userScore++;
+	   }else if BotPick == "rock" && pick == "scissors"{
+		fmt.Println("You lose!")
+		BotScore++;
+	   }else if BotPick == "rock" && pick == "paper"{
+		fmt.Println("You win!")
+		userScore++;
+	   }else if BotPick == "scissors" && pick == "rock"{
+		fmt.Println("You win!")
+		userScore++;
+	   }else if BotPick == "scissors" && pick == "paper"{
+		fmt.Println("You lose!")
+		BotScore++;
+	   }else{
+		fmt.Println("Draw!")
+	}
+	if(BotScore == 5 && userScore < 5){
+		fmt.Println("You LOSE")
+		fmt.Println("User Score is : ", userScore)
+		fmt.Println("Bot Score is : ", BotScore)
+		os.Exit(0)
+	}else if(userScore == 5 && BotScore < 5){
+		fmt.Println("You WIN")
+		fmt.Println("User Score is : ", userScore)
+	    fmt.Println("Bot Score is : ", BotScore)
+		os.Exit(0)
+	}
+	
 }
-func Random(){
-value := rand.Intn(6)
-fmt.Print(value)
 }
+	
+
+}
+	
+	
+
