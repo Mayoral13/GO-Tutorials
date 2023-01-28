5ca7b6e7954776abe1c643173fef41714eba116d
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-01-28 23:16:30 +0100

    Created a simple quiz and learnt about recursion

--- 1.go
+++ 1.go
@@ -1,99 +1,80 @@
 /*
-7. To-Do List
-Creating a to-do list is a great beginner programming project that can be completed in
-just about any programming language. Completing a to-do list in JavaScript is a great
-beginner project for anyone interested in web development.
-Your to-do list will need to allow users to add, complete, and delete tasks.
+JUST A RANDOM QUIZ GAME
 */
 package main
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
-	"log"
-	"net/http"
-	"github.com/gorilla/mux"
+	"os"
+	"strings"
 )
- type Task struct{
-	TaskID string `json:"TaskID"`;
-	Taskname string `json:"TaskName"`;
-	Completed string `json:"Completed"`;
- }
- var tasks[]Task;
+    var words = [1]string{"boy"}
+	var lives int = 3;
+	var guesses int = 3;
+	var enteredGame bool;
 func main(){
-	tasks = append(tasks, Task{TaskID:"1",Taskname:"Cook",Completed:"false"});
-	mux := mux.NewRouter();
-	http.Handle("/",mux)
-	mux.HandleFunc("/Add",AddTask).Methods("POST")
-	mux.HandleFunc("/View",ViewTask).Methods("GET")
-	mux.HandleFunc("/View/{id}",ViewOneTask).Methods("GET")
-	mux.HandleFunc("/Update/{id}",UpdateTask).Methods("PUT")
-	mux.HandleFunc("/Delete/{id}",DeleteTask).Methods("DELETE")
-	mux.HandleFunc("/Complete/{id}",CompleteTask).Methods("PUT")
-	fmt.Println("WELCOME TO TODO LIST")
-	if err:=http.ListenAndServe(":3000",nil);err!=nil{
-		log.Fatal(err)
-	}
-
-
-}
-func AddTask(w http.ResponseWriter, r *http.Request){
-w.Header().Set("Content-Type","application/json")
-var data Task;
-json.NewDecoder(r.Body).Decode(&data)
-tasks = append(tasks,data)
-json.NewEncoder(w).Encode(data)
-}
-func ViewTask(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type","application/json")
-	json.NewEncoder(w).Encode(tasks)
-}
-func ViewOneTask(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type","application/json")
-	ID := mux.Vars(r);
-	for _,task := range tasks{
-	if(task.TaskID == ID["id"]){
-		json.NewEncoder(w).Encode(task)
-	}
+	fmt.Println("Welcome to a game of hangman")
+	fmt.Println("Enter 1 to start game")
+	for lives != 0{
+	input := bufio.NewReader(os.Stdin)
+	value,_ := input.ReadString('\n')
+	text := strings.TrimSpace(value);
+	if(text == "1"){
+		fmt.Println("You have started the game")
+		enteredGame = true;
+		game()
+	}else{
+		fmt.Println("INVALID INPUT")
 	}
 }
-func UpdateTask(w http.ResponseWriter,r *http.Request){
-	w.Header().Set("Content-Type","application/json")
-	ID := mux.Vars(r);
-	for id,task := range tasks{
-	if(task.TaskID == ID["id"]){
-		tasks = append(tasks[:id],tasks[id + 1:]... )
-		var data Task;
-		json.NewDecoder(r.Body).Decode(&data)
-		tasks = append(tasks,data)
-		json.NewEncoder(w).Encode(data)
-	}
-	}
-
 }
-func CompleteTask(w http.ResponseWriter,r *http.Request){
-	w.Header().Set("Content-Type","application/json")
-	ID := mux.Vars(r);
-	for id,task := range tasks{
-	if(task.TaskID == ID["id"] && task.Completed != "completed"){
-		tasks = append(tasks[:id],tasks[id + 1:]... )
-		tasks = append(tasks,Task{TaskID:task.TaskID,Taskname:task.Taskname,Completed:"completed"})
-		json.NewEncoder(w).Encode(tasks)
-	}else if(task.TaskID == ID["id"] && task.Completed == "completed"){
-      json.NewEncoder(w).Encode("TASK IS COMPLETED")
-		}
+func game(){
+   for enteredGame{
+	gameOver()
+	fmt.Println("LIVES LEFT ARE : ", lives)
+	fmt.Println("GUESSES LEFT ARE : ", guesses)
+	fmt.Println("First question is what is a young male?")
+	input := bufio.NewReader(os.Stdin)
+	value,_ := input.ReadString('\n')
+	text := strings.TrimSpace(value);
+     if(text == "boy"){
+      fmt.Println("WELLDONE")
+	  gameOver()
+}else{
+	fmt.Println("WRONG")
+	lives--;
+	fmt.Println("Do you want to use a guess ?")
+	fmt.Println("Press y for yes n for no")
+	input := bufio.NewReader(os.Stdin)
+	value,_ := input.ReadString('\n')
+	text := strings.TrimSpace(value);
+	if(text == "y"){
+		guessOver()
+		fmt.Println("It rhymes with soil")
+		lives++;
+		guesses--;
+	}else if(text == "n" && lives != 0){
+		fmt.Println("Try again")
+         game()
+	}else if(text == "n"){
+         game()
+	}else{
+		fmt.Println("Invalid input")
+		
 	}
-
 }
-func DeleteTask(w http.ResponseWriter,r *http.Request){
-	w.Header().Set("Content-Type","application/json")
-	ID := mux.Vars(r);
-	for id,task := range tasks{
-	if(task.TaskID == ID["id"]){
-		tasks = append(tasks[:id],tasks[id + 1:]... )
-	}
-	}
-
 }
-
-
+} 
+func gameOver(){
+	if(lives == 0){
+		fmt.Println("GAME OVER")
+		os.Exit(0)
+	}
+}
+func guessOver(){
+	if(guesses == 0){
+		fmt.Println("You cannot use any more guesses")
+		game()
+	}
+}
